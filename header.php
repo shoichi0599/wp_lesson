76d22cb84208620f559c4dc2f6e5554a57af45a5
shoichisato <shoichi.programming@gmail.com>
2018-10-22 00:51:50 +0900

    Add header image

--- header.php
+++ header.php
@@ -22,6 +22,7 @@
         <p><?php bloginfo( 'description' ); ?></p>
       </div>
     </div>
+    <img src="<?php echo get_template_directory_uri(); ?>/header-1500x250.jpg" width="1500" height="250" alt="">
     <nav>
       <div class="container">
         <?php wp_nav_menu( 'theme_location=navigation' ); ?>
